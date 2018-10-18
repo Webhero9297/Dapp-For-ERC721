6b3cd4c0f7b734e2013adedcff157bb49e2c2362
Webhero9297 <webhero9297@mail.com>
2018-10-18 10:28:06 +0800

    webhero9297

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+DB: MongoDB
+Auth: OK
+Project: blank
+Date: 2017-12-18
---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.less('resources/assets/less/fileupload.less', 'public/css')
+   .less('resources/assets/less/table.less', 'public/css')
+   .react('resources/assets/js/app.js', 'public/js')
+   .react('resources/assets/js/Blockchain/Blockchain.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
